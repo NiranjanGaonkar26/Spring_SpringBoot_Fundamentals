442f98044ca1b56bd884c92d5056f45911187181
Niranjan G C <niranjangc99@gmail.com>
2025-09-12 22:19:13 +0530

    Understanding Eager vs Lazy instantiation of singleton beans

--- Ex6_Lec40/src/main/java/com/example6/main/Application.java
+++ Ex6_Lec40/src/main/java/com/example6/main/Application.java
@@ -13,14 +13,18 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        VehicleServices vs1 = context.getBean(VehicleServices.class);
-        VehicleServices vs2 = context.getBean("vehicleServices", VehicleServices.class);
+//        VehicleServices vs1 = context.getBean(VehicleServices.class);
+//        VehicleServices vs2 = context.getBean("vehicleServices", VehicleServices.class);
+//
+//        System.out.println("Hash Code of the vs1 bean is: "+vs1.hashCode());
+//        System.out.println("Hash Code of the vs2 bean is: "+vs2.hashCode());
+//
+//        if(vs1 == vs2) {
+//            System.out.println("Only one instance of the VehicleServices bean is maintained, and the same will be referenced every time");
+//        }
 
-        System.out.println("Hash Code of the vs1 bean is: "+vs1.hashCode());
-        System.out.println("Hash Code of the vs2 bean is: "+vs2.hashCode());
-
-        if(vs1 == vs2) {
-            System.out.println("Only one instance of the VehicleServices bean is maintained, and the same will be referenced every time");
-        }
+        System.out.println("This line will be printed before the Person bean creation");
+        Person person = context.getBean(Person.class);
+        System.out.println("This line will always be printed after the Person bean creation");
     }
 }
