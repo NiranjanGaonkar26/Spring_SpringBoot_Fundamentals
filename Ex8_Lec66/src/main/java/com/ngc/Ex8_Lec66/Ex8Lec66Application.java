2acde8605b0e3a1ebd73fe1b94d4f5cf1f2c47f2
Niranjan G C <niranjangc99@gmail.com>
2025-09-25 16:24:19 +0530

    Intialized a simple Hello World Web App using Spring Boot

---
+++ Ex8_Lec66/src/main/java/com/ngc/Ex8_Lec66/Ex8Lec66Application.java
@@ -0,0 +1,13 @@
+package com.ngc.Ex8_Lec66;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex8Lec66Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex8Lec66Application.class, args);
+	}
+
+}
---
+++ Ex8_Lec66/src/test/java/com/ngc/Ex8_Lec66/Ex8Lec66ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngc.Ex8_Lec66;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex8Lec66ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
