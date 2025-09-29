adbbd1ec04ed5c783a35520a1230df093320ab5b
Niranjan G C <niranjangc99@gmail.com>
2025-09-29 15:08:53 +0530

    Added about and contact pages, explored processing data sent from UI to the backend using RequestParam

---
+++ Ex10_Lec78/src/main/java/com/ngc/Ex10_Lec78/Controller/ContactController.java
@@ -0,0 +1,29 @@
+package com.ngc.Ex10_Lec78.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.logging.Logger;
+
+@Controller
+public class ContactController {
+
+    Logger log = Logger.getLogger(ContactController.class.getName());
+
+    @RequestMapping("/contact")
+    public String displayContact(){
+        return "contact";
+    }
+
+    @RequestMapping("/saveMsg")
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message){
+        log.info(name);
+        log.info(mobileNum);
+        log.info(email);
+        log.info(subject);
+        log.info(message);
+        return new ModelAndView("redirect:/contact");
+    }
+}
