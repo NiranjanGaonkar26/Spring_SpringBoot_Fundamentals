77fd2aa22c74632cf644741afd62aabd41613dfd
Niranjan G C <niranjangc99@gmail.com>
2025-10-11 22:49:16 +0530

    Added a new example to explore Global Exception Handling using @ControllerAdvice and @ExceptionHandler

---
+++ Ex15_Lec122/src/main/java/com/ngc/Ex15_Lec122/Controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.ngc.Ex15_Lec122.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+    public String handleLogin(@RequestParam(value = "error", required = false) boolean error, @RequestParam(value = "logout", required = false) boolean logout, Model model){
+        String errorMsg = null;
+        if (error){
+            errorMsg = "Login failed! Username or Password is incorrect, please try again";
+        } else if (logout) {
+            errorMsg = "You have been successfully logged out!!";
+        }
+        model.addAttribute("errorMessge", errorMsg);
+        return "login.html";
+    }
+}
