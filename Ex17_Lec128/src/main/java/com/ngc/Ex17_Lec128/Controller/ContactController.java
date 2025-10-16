1e4b3a89cfedbfcefb01657bbf4faff1d8b370c2
Niranjan G C <niranjangc99@gmail.com>
2025-10-16 11:41:13 +0530

    Added a new example to explore h2 DB, spring JDBC. Created a table to store data from Contact page

---
+++ Ex17_Lec128/src/main/java/com/ngc/Ex17_Lec128/Controller/ContactController.java
@@ -0,0 +1,53 @@
+package com.ngc.Ex17_Lec128.Controller;
+
+import com.ngc.Ex17_Lec128.Model.Contact;
+import com.ngc.Ex17_Lec128.Service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Slf4j
+@Controller
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContact(Model model){
+        model.addAttribute("contact", new Contact());
+        return "contact";
+    }
+
+//    @RequestMapping("/saveMsg", method = POST)
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message){
+//        log.info(name);
+//        log.info(mobileNum);
+//        log.info(email);
+//        log.info(subject);
+//        log.info(message);
+//        return new ModelAndView("redirect:/contact");
+//    }
+
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+        if (errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact";
+        }
+        contactService.saveMessageDetails(contact);
+        contactService.setCounter(contactService.getCounter()+1);
+        log.info("Number of times contact form is submitted: "+contactService.getCounter());
+        return "redirect:/contact";
+    }
+}
---
+++ Ex17_Lec128/src/test/java/com/ngc/Ex8_Lec66/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngc.Ex8_Lec66;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
