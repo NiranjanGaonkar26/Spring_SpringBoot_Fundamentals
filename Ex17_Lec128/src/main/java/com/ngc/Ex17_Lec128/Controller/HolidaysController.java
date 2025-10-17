19aaf2aaa08f1be4469976ad6b06f739acac3a43
Niranjan G C <niranjangc99@gmail.com>
2025-10-17 22:35:25 +0530

    Moved holidays list to h2 DB - insert at startup, and fetch the holidays list from the DB

--- Ex17_Lec128/src/main/java/com/ngc/Ex17_Lec128/Controller/HolidaysController.java
+++ Ex17_Lec128/src/main/java/com/ngc/Ex17_Lec128/Controller/HolidaysController.java
@@ -1,6 +1,8 @@
 package com.ngc.Ex17_Lec128.Controller;
 
 import com.ngc.Ex17_Lec128.Model.Holiday;
+import com.ngc.Ex17_Lec128.Repository.HolidaysRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,9 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+
+    @Autowired
+    private HolidaysRepository holidaysRepository;
 
 //    @GetMapping(value = "/holidays")
 //    public String displayHolidays(@RequestParam(required = true) boolean festival,@RequestParam(required = false) boolean federal, Model model){
@@ -49,16 +54,7 @@ public class HolidaysController {
             model.addAttribute("federalParam", true);
         }
 
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.fetchHolidaysFromDB();
 
         Holiday.Type[] types = Holiday.Type.values();
         for(Holiday.Type type : types){
---
+++ Ex17_Lec128/src/main/java/com/ngc/Ex17_Lec128/Repository/HolidaysRepository.java
@@ -0,0 +1,27 @@
+package com.ngc.Ex17_Lec128.Repository;
+
+import com.ngc.Ex17_Lec128.Model.Holiday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class HolidaysRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public HolidaysRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Holiday> fetchHolidaysFromDB(){
+        String sql = "SELECT * FROM HOLIDAYS";
+        var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+}
