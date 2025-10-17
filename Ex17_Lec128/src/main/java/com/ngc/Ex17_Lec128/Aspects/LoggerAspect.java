05ba9a6fc29f92db85daae7b960cf6f418e3a837
Niranjan G C <niranjangc99@gmail.com>
2025-10-17 21:57:42 +0530

    Added aspect logic to log exception from anywhere within the application

---
+++ Ex17_Lec128/src/main/java/com/ngc/Ex17_Lec128/Aspects/LoggerAspect.java
@@ -0,0 +1,18 @@
+package com.ngc.Ex17_Lec128.Aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @AfterThrowing(value = "execution(* com.ngc.Ex17_Lec128..*.*(..))",throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.error(joinPoint.getSignature()+ " An exception happened due to : "+ex.getMessage());
+    }
+}
